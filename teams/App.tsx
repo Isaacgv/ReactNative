147a96954643294a57aa96c3d9c28af1278f7566
Isaac <isaacgonzalesv@gmail.com>
2024-12-07 15:24:38 +0100

    add fonts ans loading component

--- teams/App.tsx
+++ teams/App.tsx
@@ -1,7 +1,16 @@
-import { Groups } from './src/screens/Groups';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import theme from './src/theme'
+
+import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      { fontsLoaded ? <Groups/> : <Loading/> }
+    </ThemeProvider>
   );
 }

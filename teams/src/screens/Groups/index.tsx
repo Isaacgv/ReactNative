0d962df272976f49ff505b48389b865e71c313ae
Isaac <isaacgonzalesv@gmail.com>
2024-12-09 21:37:48 +0100

    add list groups

--- teams/src/screens/Groups/index.tsx
+++ teams/src/screens/Groups/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
@@ -5,6 +7,8 @@ import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galery']);
+
   return (
     <Container>
       <Header/>
@@ -12,8 +16,15 @@ export function Groups() {
         title="Group"
         subtitle="Play with them"
       />
-       <GroupCard 
-        title="Galery" />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+      />
     </Container>
   );
 }

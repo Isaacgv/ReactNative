db64320f523e8901bc03a36c42a72f5a0478f96a
Isaac <isaacgonzalesv@gmail.com>
2024-12-30 09:18:06 +0100

    add compunent Input

--- teams/src/screens/NewGroup/index.tsx
+++ teams/src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -16,9 +17,13 @@ export function NewGroup() {
           title="New Group"
           subtitle="create the class to add people"
         />
+        <Input 
+            placeholder="Group name"
+        />
 
         <Button 
           title="Create"
+          style={{ marginTop: 20 }}
         />
       </Content>
     </Container>

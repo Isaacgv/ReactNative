a048cc72e469cfcde7c36645d126055f5d7bb31f
Isaac <isaacgonzalesv@gmail.com>
2025-01-08 16:29:26 +0100

    add gym project

---
+++ app/App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar, Text, View } from 'react-native'
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from '@expo-google-fonts/roboto'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
+
+  return (
+    <View
+    style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#202024',
+    }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      
+      {fontsLoaded ? <Text>Home</Text> : <View />}
+    </View>
+  );
+}
+
---
+++ app/README.md
@@ -0,0 +1,5 @@
+# Start new project 
+    npx create-expo app --template
+
+# Fonts
+    npx expo install expo-font @expo-google-fonts/roboto

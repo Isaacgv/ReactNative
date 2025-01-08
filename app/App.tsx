409255503b9f1ea3b6107e153ae35ebd59704286
Isaac <isaacgonzalesv@gmail.com>
2025-01-08 16:57:45 +0100

    add loading component

--- app/App.tsx
+++ app/App.tsx
@@ -1,30 +1,34 @@
-import { StatusBar, Text, View } from 'react-native'
+import { StatusBar } from 'react-native'
 import {
   useFonts,
   Roboto_700Bold,
   Roboto_400Regular,
 } from '@expo-google-fonts/roboto'
 
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
+import { config } from './config/gluestack-ui.config'
+import { Loading } from '@components/Loading'
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
 
   return (
-    <View
-    style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#202024',
-    }}
-    >
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      
-      {fontsLoaded ? <Text>Home</Text> : <View />}
-    </View>
+    <GluestackUIProvider config={config}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+
+        {fontsLoaded ? (
+          <Center flex={1} bg="$gray700">
+            <Text>Home</Text>
+          </Center>
+        ) : (
+          <Loading />
+        )}
+
+    </GluestackUIProvider>
   );
 }
 
--- app/README.md
+++ app/README.md
@@ -3,3 +3,10 @@
 
 # Fonts
     npx expo install expo-font @expo-google-fonts/roboto
+
+# Install gluestack
+    npm install @gluestack-ui/themed@1.1.34 @gluestack-style/react@1.0.57 @gluestack-ui/config@1.1.19 --legacy-peer-deps
+    npx expo install react-native-svg
+
+# Create folder theme config
+    npx gluestack-ui-scripts eject-theme

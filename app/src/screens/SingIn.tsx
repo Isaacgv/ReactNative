3035626509d95188e69093f98f1ff6e3209fef8d
Isaac <isaacgonzalesv@gmail.com>
2025-01-10 08:36:31 +0100

    add Inputs

--- app/src/screens/SingIn.tsx
+++ app/src/screens/SingIn.tsx
@@ -1,6 +1,8 @@
-import { Center, Image, Text, VStack } from '@gluestack-ui/themed'
+import { Center, Heading, Image, Text, VStack } from '@gluestack-ui/themed'
 import BackgroundImg from '@assets/background.png'
 import Logo from '@assets/logo.svg'
+import { Input } from '@components/Input'
+
 
 export function SignIn() {
   return (
@@ -14,12 +16,19 @@ export function SignIn() {
         position="absolute"
       />
 
-        <Center my="$24">
-            <Logo />
-            <Text color="$gray100" fontSize="$sm">
-                Train your mind and body
-            </Text>
-        </Center>
+        <VStack flex={1} px="$10" pb="$16">
+            <Center my="$24">
+                <Logo />
+                <Text color="$gray100" fontSize="$sm">
+                    Train your mind and body
+                </Text>
+            </Center>
+            <Center gap="$2">
+                <Heading color="$gray100">Login</Heading>
+                <Input placeholder="E-mail" />
+                <Input placeholder="Password" />
+            </Center>
+      </VStack>
 
     </VStack>
   )

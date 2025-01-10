1c6b3762e6434e2c0f2ed9a8c5453e145fc76593
Isaac <isaacgonzalesv@gmail.com>
2025-01-10 09:03:40 +0100

    modify SignIn

--- app/src/screens/SingIn.tsx
+++ app/src/screens/SingIn.tsx
@@ -1,39 +1,46 @@
-import { Center, Heading, Image, Text, VStack } from '@gluestack-ui/themed'
+import { Center, Heading, Image, ScrollView, Text, VStack } from '@gluestack-ui/themed'
 import BackgroundImg from '@assets/background.png'
 import Logo from '@assets/logo.svg'
 import { Input } from '@components/Input'
+import { Button } from '@components/Button'
 
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="$gray700">
-      <Image
-        w="$full"
-        h={624}
-        source={BackgroundImg}
-        defaultSource={BackgroundImg}
-        alt="Persons training"
-        position="absolute"
-      />
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+        <VStack flex={1} bg="$gray700">
+        <Image
+            w="$full"
+            h={624}
+            source={BackgroundImg}
+            defaultSource={BackgroundImg}
+            alt="Persons training"
+            position="absolute"
+        />
 
-        <VStack flex={1} px="$10" pb="$16">
-            <Center my="$24">
-                <Logo />
-                <Text color="$gray100" fontSize="$sm">
-                    Train your mind and body
-                </Text>
-            </Center>
-            <Center gap="$2">
-                <Heading color="$gray100">Login</Heading>
-                <Input
-                    placeholder="E-mail"
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                />
-                <Input placeholder="Password" secureTextEntry/>
-            </Center>
-      </VStack>
+            <VStack flex={1} px="$10" pb="$16">
+                <Center my="$24">
+                    <Logo />
+                    <Text color="$gray100" fontSize="$sm">
+                        Train your mind and body
+                    </Text>
+                </Center>
+                <Center gap="$2">
+                    <Heading color="$gray100">Login</Heading>
+                    <Input
+                        placeholder="E-mail"
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                    />
+                    <Input placeholder="Password" secureTextEntry/>
+                    <Button title="Access" />
+                </Center>
+        </VStack>
 
-    </VStack>
+        </VStack>
+    </ScrollView>
   )
 }

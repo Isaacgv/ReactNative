7895b2c2dd710a262459b2a62ee12100088cb689
Isaac <isaacgonzalesv@gmail.com>
2025-01-10 09:09:51 +0100

    modify SignUp

--- app/src/screens/SingUp.tsx
+++ app/src/screens/SingUp.tsx
@@ -22,28 +22,29 @@ import {
             h={624}
             source={BackgroundImg}
             defaultSource={BackgroundImg}
-            alt="Pessoas treinando"
+            alt="Persons training"
             position="absolute"
           />
           <VStack flex={1} px="$10" pb="$16">
             <Center my="$24">
               <Logo />
               <Text color="$gray100" fontSize="$sm">
-                Treine sua mente e seu corpo
+              Train your mind and body
               </Text>
             </Center>
             <Center flex={1} gap="$2">
-              <Heading color="$gray100">Crie sua conta</Heading>
+              <Heading color="$gray100">Create your account
+              </Heading>
               <Input placeholder="Nome" />
               <Input
                 placeholder="E-mail"
                 keyboardType="email-address"
                 autoCapitalize="none"
               />
-              <Input placeholder="Senha" secureTextEntry />
-              <Button title="Criar e acessar" />
+              <Input placeholder="Password" secureTextEntry />
+              <Button title="Create and access" />
             </Center>
-            <Button title="Voltar para o login" variant="outline" mt="$12" />
+            <Button title="Back to login" variant="outline" mt="$12" />
           </VStack>
         </VStack>
       </ScrollView>

55978182e07cb55874d44a37f53261d6c00304ae
Isaac <isaacgonzalesv@gmail.com>
2025-01-10 07:56:52 +0100

    add background image

--- app/App.tsx
+++ app/App.tsx
@@ -5,9 +5,10 @@ import {
   Roboto_400Regular,
 } from '@expo-google-fonts/roboto'
 
-import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
+import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from '@components/Loading'
+import { SignIn } from '@screens/SignIn'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
@@ -19,15 +20,9 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-
-        {fontsLoaded ? (
-          <Center flex={1} bg="$gray700">
-            <Text>Home</Text>
-          </Center>
-        ) : (
-          <Loading />
-        )}
-
+        
+        {fontsLoaded ? <SignIn /> : <Loading />}
+        
     </GluestackUIProvider>
   );
 }
